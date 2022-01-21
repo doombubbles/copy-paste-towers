de6ec66b4cfb6aa8862691c008ad06697d7469f6
doombubbles <obsidiantoolzg@gmail.com>
2022-01-20 19:53:00 -0800

    Initial Commit

---
+++ ModHelperData.cs
@@ -0,0 +1,16 @@
+﻿namespace CopyPasteTowers
+{
+    public static class ModHelperData
+    {
+        public const string Version = "1.0.0";
+        public const string Name = "Copy/Paste Towers";
+
+        public const string Description =
+            "When you press Ctrl+C while selecting a tower you will copy it, showing you its total cost. " +
+            "Then, pressing Ctrl+V will start the placement for a copy of it with the same upgrades, paying the full combined price. " +
+            "Helpful for doing paragon sacrifieces.";
+
+        public const string RepoOwner = "doombubbles";
+        public const string RepoName = "copy-paste-towers";
+    }
+}
